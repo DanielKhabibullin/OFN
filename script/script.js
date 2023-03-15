defe5e0b74ccaeee19758997e43908f54525656e
Daniel Khabibullin <i6unny@gmail.com>
2023-03-15 22:00:39 +0500

    Add html/css

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const choicesElem = document.querySelector('.js-choice');
+
+const choises = new Choices(choicesElem, {
+	searchEnabled: false,
+	shouldSort: false,
+	itemSelectText: '',
+	classNames: {
+		containerInner: 'choices__inner choices-box',
+	}
+});
